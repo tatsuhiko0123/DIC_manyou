a70415e7a96926f3e0858a16bac881e2bac3db1a
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-17 13:00:49 +0900

    編集画面作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,10 +18,20 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
