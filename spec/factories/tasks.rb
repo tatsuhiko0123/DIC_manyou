dff5f66ddb6479fd6b0fd5bcf71f15ab9dd28a90
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-18 01:30:55 +0900

    作成日時の降順での並び替えのテストを実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
   # 作成するテストデータの名前を「task」とします
   # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
   factory :task do
-    title { 'Factoryで作ったデフォルトのタイトル１' }
-    content { 'Factoryで作ったデフォルトのコンテント１' }
+    title { 'test1' }
+    content { 'test1' }
   end
-  factory :task1, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル１' }
-    content { 'Factoryで作ったデフォルトのコンテント１' }
+  factory :task2, class: Task do
+    title { 'test2' }
+    content { 'test2' }
   end
 end
