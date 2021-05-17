c901113371c318b0db32cd105da33f5932c71adc
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-17 13:24:23 +0900

    削除機能作成、フラッシュメッセージ表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -18,15 +19,12 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path, notice: "タスクを編集しました！"
     else
@@ -34,8 +32,16 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスクを削除しました！"
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :content)
+  end
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
