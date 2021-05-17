e935eb6e5ad08b963ccb9c7de112071f2fd46a31
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-17 12:50:29 +0900

    新規画面作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,17 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました！"
+    else
+      render :new
+    end
+
   end
 
   def show
@@ -11,5 +22,10 @@ class TasksController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
