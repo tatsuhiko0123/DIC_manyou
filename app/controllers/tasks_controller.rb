504759efbff0e3d93d940a7e9460df2edefbd0ba
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-20 15:42:44 +0900

    ページネーション実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,19 +2,19 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     if params[:sort_expired]
-      @tasks = Task.order(expired_at: :desc)
+      @tasks = Task.order(expired_at: :desc).page(params[:page]).per(3)
     elsif params[:sort_priority]
-      @tasks = Task.order(priority: :asc)      
+      @tasks = Task.order(priority: :asc).page(params[:page]).per(3)     
     elsif params[:search]
       if params[:search_title].present? && params[:search_status].present?
-        @tasks = Task.search_title(params[:search_title]).search_status(params[:search_status])
+        @tasks = Task.search_title(params[:search_title]).search_status(params[:search_status]).page(params[:page]).per(3) 
       elsif params[:search_title].present?
-        @tasks = Task.search_title(params[:search_title])
+        @tasks = Task.search_title(params[:search_title]).page(params[:page]).per(3) 
       elsif params[:search_status].present?
-        @tasks = Task.search_status(params[:search_status])
+        @tasks = Task.search_status(params[:search_status]).page(params[:page]).per(3) 
       end
     else
-      @tasks = Task.order(created_at: :desc)
+      @tasks = Task.order(created_at: :desc).page(params[:page]).per(3) 
     end
   end
 
