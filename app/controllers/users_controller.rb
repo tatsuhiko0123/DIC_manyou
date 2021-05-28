41e9cdaf5a0058f68a29a01d690c7506f177207d
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-28 19:26:11 +0900

    タスク一覧画面に作成者追加、usersコントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to tasks_path
+    end
   end
   private
   def user_params
